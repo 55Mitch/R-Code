4f57c92a67d02fe10834f4d6bdd4baa7db4f1c4b
55Mitch <55Mitch@users.noreply.github.com>
2018-03-26 10:31:28 -0400

    Update Cred.R

--- Cred.R
+++ Cred.R
@@ -1,5 +1,5 @@
 ### fetching tweets ###
-download.file(url = "http://curl.haxx.se/ca/cacert.pem",destfile = "cacert.pem")
+#download.file(url = "http://curl.haxx.se/ca/cacert.pem",destfile = "cacert.pem")
 ###### Load Plotly Credentials
 Sys.setenv("plotly_username"="55mitch")
 Sys.setenv("plotly_api_key"="ktOOlYRAf2sxMF3nahed")
