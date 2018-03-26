8e476c8e4ad125ef456c978675e223c9c832802a
55Mitch <55Mitch@users.noreply.github.com>
2018-03-26 09:40:11 -0400

    Update Cred.R

--- Cred.R
+++ Cred.R
@@ -1,8 +1,3 @@
-###Loading my twitter credentials
-api_key <- 'NUS7NG4UW8jgUPKyzo2zfGjZj'
-api_secret <- 'etmI4Pd3QAHfTX6YalYAqyakCtlPIfXztlz8yuMjkErWpBlkCQ'
-
-create_token(app = "mytwitterapp", api_key, api_secret,set_renv = TRUE)
 ### fetching tweets ###
 download.file(url = "http://curl.haxx.se/ca/cacert.pem",destfile = "cacert.pem")
 ###### Load Plotly Credentials
