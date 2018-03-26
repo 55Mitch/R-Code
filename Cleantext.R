59d24e91442aa9af884169ffbb657d81019340e1
55Mitch <55Mitch@users.noreply.github.com>
2018-03-26 12:27:00 -0400

    Update Cleantext.R

--- Cleantext.R
+++ Cleantext.R
@@ -26,26 +26,23 @@ clean.text <- function(some_txt)
   names(some_txt) = NULL
   return(some_txt)
 }
-  f_clean_tweets <- function (tweets) {
-  
-  clean_tweets = sapply(tweets, function(x) x[[text]])
-  # remove retweet entities
-  clean_tweets = gsub('(RT|via)((?:\\b\\W*@\\w+)+)', '', clean_tweets)
-  # remove at people
-  clean_tweets = gsub('@\\w+', '', clean_tweets)
-  # remove punctuation
-  clean_tweets = gsub('[[:punct:]]', '', clean_tweets)
-  # remove numbers
-  clean_tweets = gsub('[[:digit:]]', '', clean_tweets)
-  # remove html links
-  clean_tweets = gsub('http\\w+', '', clean_tweets)
-  # remove unnecessary spaces
-  clean_tweets = gsub('[ \t]{2,}', '', clean_tweets)
-  clean_tweets = gsub('^\\s+|\\s+$', '', clean_tweets)
-  # remove emojis or special characters
-  clean_tweets = gsub('<.*>', '', enc2native(clean_tweets))
-  
-  clean_tweets = tolower(clean_tweets)
-  
-  clean_tweets
+ clean_tweet_text <- function(x) {
+
+  require(stringi)
+
+  c(
+    url_pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+    handle_pattern = "(^|[^@[:word:]])@([:word:]{1,15})\b",
+    entity_pattern = "&[^[:space:]]*;",
+    other_pattern = "[[:punct:][:cntrl:][:digit:]]",
+    twitter_hashtag_regex <- "(^|[^&\\p{L}\\p{M}\\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7])(#|\uFF03)(?!\uFE0F|\u20E3)([\\p{L}\\p{M}\\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7]*[\\p{L}\\p{M}][\\p{L}\\p{M}\\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7]*)"
+  ) -> pats
+
+  pats <- sprintf("(%s)", paste0(pats, collapse="|"))
+
+  x <- stri_replace_all_regex(x, pats, "")
+  x <- stri_trim_both(x)
+
+  x
+
 }
