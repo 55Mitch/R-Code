c8fe9ad07173586990c8f3ad47b8eb194a2285b5
55Mitch <55Mitch@users.noreply.github.com>
2018-03-29 07:29:02 -0400

    Delete Cred.R

--- Cred.R
+++
@@ -1,9 +0,0 @@
-### fetching tweets ###
-###Loading my twitter credentials
-api_key <- "NUS7NG4UW8jgUPKyzo2zfGjZj"
-api_secret <- "etmI4Pd3QAHfTX6YalYAqyakCtlPIfXztlz8yuMjkErWpBlkCQ"
-twitter_token <- create_token(app = "mytwitterapp", api_key, api_secret)
-#download.file(url = "http://curl.haxx.se/ca/cacert.pem",destfile = "cacert.pem")
-###### Load Plotly Credentials
-Sys.setenv("plotly_username"="55mitch")
-Sys.setenv("plotly_api_key"="ktOOlYRAf2sxMF3nahed")
